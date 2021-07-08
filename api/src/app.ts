ec2188d67459177c603eaa22b269f9d07821931e
Nick Wylynko <nick@blueskylabs.com.au>
2021-07-08 14:19:49 +0800

    use pg instead

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,11 @@
 import Fastify from "fastify";
-import { db } from "./db"
+import { client } from "./db"
 
 export const app = Fastify({ logger: true });
 
 app.get("/", async (req, res) => {
+
+  const data = await (await client).query("SELECT * FROM accounts")
   
-  return { database: db.isConnected }
+  return { data }
 });
--- api/src/db.ts
+++ api/src/db.ts
@@ -1,20 +1,20 @@
-import { createConnection, Connection, ConnectionOptions } from "typeorm";
+import { Pool, PoolClient } from "pg";
 
-export let db: Connection;
 
 interface ENV {
   [x: string]: string;
 }
 
-export const openDB = async () => {
-  const { DB_HOST } = process.env as ENV
-  db = await createConnection({
-    type: "cockroachdb",
-    host: DB_HOST || "localhost",
-    port: 26257,
-    username: "api",
-    // password: "test",
-    database: "bank"
-  });
-  return db
-}
+const { DB_HOST } = process.env as ENV;
+
+const config = {
+  user: "api",
+  host: DB_HOST || "localhost",
+  database: "bank",
+  port: 26257,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+export const client = (new Pool(config)).connect();
